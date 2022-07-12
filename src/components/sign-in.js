38360acbbb936b929dc1a0c68e9833e6af90ac08
dmwspace <dmwspace@gmail.com>
2022-07-11 22:21:27 -0700

    Started SignIn page

---
+++ src/components/sign-in.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SignIn = () => {
+    return (
+        <div>
+            <input 
+                type="text"  
+                placeholder='Enter username here'
+            />
+            <input
+                type="password"
+                placeholder='Enter password here'
+            />
+        </div>
+
+    )
+}
+export default SignIn
